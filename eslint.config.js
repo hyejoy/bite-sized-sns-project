f0f9ecc295bded1029e69a58e5b953880970ccb9
hyejoy <devchoi97@gmail.com>
2026-01-28 08:43:25 +0900

    feat: init project with Vite 6.5.0 and TypeScript

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-unused-vars": "warn", // 사용하지 않는 변수 error 표시하는 옵션
+      "@typescript-eslint/no-explicit-any": "off", // 명시적 특정 변수의 any타입 정의 할수없도록 하는 옵션
+    },
+  },
+);
