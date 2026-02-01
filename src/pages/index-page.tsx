c4b0260b2fcc6a1f263c84b6fef0f3cd32615c72
hyejoy <devchoi97@gmail.com>
2026-02-01 19:26:31 +0900

    feat: Tailwind CSS 클래스 자동 정렬 설정 추가
    - .prettierrc 파일 생성 및 플러그인 설정
    - prettier-plugin-tailwindcss 적용으로 유틸리티 클래스 자동 정렬 활성화

--- src/pages/index-page.tsx
+++ src/pages/index-page.tsx
@@ -4,6 +4,8 @@ import Viewer from "../components/viewer";
 export default function IndexPage() {
   return (
     <>
+      <div className="text-2xl font-bold underline">Hello World</div>
+
       <Viewer />
       <Controller />
     </>
